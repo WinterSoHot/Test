b500abf5ba930a860abf88017e1aaff9774a05aa
gudongxian <gudongxian@qq.com>
2020-08-02 17:52:11 +0800

    code(LeetCode):每日一题

---
+++ LeetCode/src/main/java/cn/dx/leetcode/Flatten.java
@@ -0,0 +1,51 @@
+package cn.dx.leetcode;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
+
+/**
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+ *
+ * @author gudongxian
+ * @version 0.1
+ * @date 8/2/2020
+ */
+public class Flatten {
+
+    public void search(TreeNode node, List<Integer> list) {
+        if (node != null) {
+            list.add(node.val);
+            if (node.left != null) {
+                search(node.left,list);
+            }
+            if (node.right != null) {
+                search(node.right,list);
+            }
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            search(root, list);
+        }else {
+            return;
+        }
+        System.out.println(list);
+        TreeNode curNode = root;
+        for (int i = 1; i < list.size(); i++) {
+            Integer v = list.get(i);
+            curNode.left = null;
+            if (curNode.right == null) {
+                curNode.right = new TreeNode();
+            }
+            curNode.right.val = v;
+            curNode = curNode.right;
+        }
+    }
+}
