b421577d444a809ef56727d839df866ebb7dae1a
gudongxian <gudongxian@qq.com>
2020-09-03 15:14:18 +0800

    :construction: BullCode 翻转链表

--- LeetCode/src/main/java/cn/dx/bullcode/ReverseList.java
+++ LeetCode/src/main/java/cn/dx/bullcode/ReverseList.java
@@ -5,6 +5,12 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ *
+ * 题目描述
+ * 输入一个链表，反转链表后，输出新链表的表头
+ *
+ * https://www.nowcoder.com/practice/75e878df47f24fdc9dc3e400ec6058ca?tpId=188&&tqId=35160&rp=1&ru=/activity/oj&qru=/ta/job-code-high-week/question-ranking
+ *
  * @author gudongxian
  * @version 0.1
  * @date 2020/9/3
