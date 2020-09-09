2d5e5a09dc5b3f657801a470e541aea8b0e6e810
gudongxian <gudongxian@qq.com>
2020-09-09 08:33:05 +0800

    :construction: LeetCode 每日一题 组合总和：回溯+剪枝

--- LeetCode/src/main/java/cn/dx/leetcode/CombinationSum.java
+++ LeetCode/src/main/java/cn/dx/leetcode/CombinationSum.java
@@ -5,6 +5,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ *
+ * candidates 中的数字可以无限制重复被选取。
+ *
+ * 说明：
+ *
+ *     所有数字（包括 target）都是正整数。
+ *     解集不能包含重复的组合。
+ *
+ * https://leetcode-cn.com/classic/problems/combination-sum/description/
+ *
+ * 难度：中
+ */
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         // 回溯 + 剪枝
