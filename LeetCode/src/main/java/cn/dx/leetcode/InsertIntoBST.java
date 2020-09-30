101c4c2c3d4f26464dc86d33f19406bf112de178
gudongxian <gudongxian@qq.com>
2020-09-30 08:23:02 +0800

    :construction: LeetCode 每日一题 二叉搜索树中的插入操作

---
+++ LeetCode/src/main/java/cn/dx/leetcode/InsertIntoBST.java
@@ -0,0 +1,78 @@
+package cn.dx.leetcode;
+
+/**
+ *
+ * 给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据保证，新值和原始二叉搜索树中的任意节点值都不同。
+ *
+ * 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insert-into-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 例如,
+ *
+ * 给定二叉搜索树:
+ *
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * 和 插入的值: 5
+ *
+ * 你可以返回这个二叉搜索树:
+ *
+ *          4
+ *        /   \
+ *       2     7
+ *      / \   /
+ *     1   3 5
+ *
+ *  或者这个树也是有效的:
+ *
+ *          5
+ *        /   \
+ *       2     7
+ *      / \
+ *     1   3
+ *          \
+ *           4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insert-into-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 难度：中等
+ *
+ * @author gudongxian
+ * @version 0.1
+ * @date 2020/9/30
+ */
+public class InsertIntoBST {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        dfs(root, val);
+        return root;
+    }
+
+    private void dfs(TreeNode node, int val) {
+        if (node.val > val) {
+            if (node.left == null){
+                node.left = new TreeNode(val);
+                return;
+            }
+            dfs(node.left, val);
+        } else {
+            if (node.right == null){
+                node.right = new TreeNode(val);
+                return;
+            }
+            dfs(node.right, val);
+        }
+    }
+
+}
