faa80a0512c03c07b287a070d050c6be33e0cc8b
gudongxian <gudongxian@qq.com>
2020-10-20 09:56:56 +0800

    :construction: LeetCode 每日一题 重排列表

---
+++ LeetCode/src/main/java/cn/dx/leetcode/ReorderList.java
@@ -0,0 +1,54 @@
+package cn.dx.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reorder-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 难度：中等
+ *
+ * @author gudongxian
+ * @version 0.1
+ * @date 2020/10/20
+ */
+public class ReorderList {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> list = new ArrayList<>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node);
+            node = node.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+
+    public static void main(String[] args) {
+        ReorderList rl = new ReorderList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        rl.reorderList(head);
+    }
+}
