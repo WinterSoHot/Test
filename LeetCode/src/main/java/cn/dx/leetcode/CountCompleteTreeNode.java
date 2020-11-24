1c5ef5114a9f06f9f56867bf1249cf10a4700965
Dongxian Gu <gudongxian@qq.com>
2020-11-24 12:05:31 +0800

    :construction: LeetCode 每日一题 完全二叉树的节点个数

---
+++ LeetCode/src/main/java/cn/dx/leetcode/CountCompleteTreeNode.java
@@ -0,0 +1,47 @@
+package cn.dx.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 给出一个完全二叉树，求出该树的节点个数。
+ * <p>
+ * 说明：
+ * <p>
+ * 完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-complete-tree-nodes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 难度：中等
+ * level: medium
+ *
+ * @author dongxian
+ * @version 0.1
+ * @date 20-11-24
+ **/
+public class CountCompleteTreeNode {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.push(root);
+        int count = 0;
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            count += sz;
+            for (int i = 0; i < sz; i++) {
+                TreeNode node = q.pop();
+                if (node.left != null) {
+                    q.push(node.left);
+                }
+                if (node.right != null) {
+                    q.push(node.right);
+                }
+            }
+        }
+        return count;
+    }
+}
