879c25264936211dee92e55cdb11f019f7a352f6
gudongxian <gudongxian@qq.com>
2020-11-28 23:01:25 +0800

    :construction: LeetCode 区间和计数

--- LeetCode/src/main/java/cn/dx/leetcode/CountRangeSum.java
+++ LeetCode/src/main/java/cn/dx/leetcode/CountRangeSum.java
@@ -36,4 +36,70 @@ public class CountRangeSum {
         }
         return ret;
     }
+
+    public int countRangeSum2(int[] nums, int lower, int upper) {
+        long s = 0;
+        // 前缀和
+        long[] sum = new long[nums.length + 1];
+        for (int i = 0; i < nums.length; ++i) {
+            s += nums[i];
+            sum[i + 1] = s;
+        }
+        return countRangeSumRecursive(sum, lower, upper, 0, sum.length - 1);
+    }
+
+    public int countRangeSumRecursive(long[] sum, int lower, int upper, int left, int right) {
+        // 二分查找
+        if (left == right) {
+            return 0;
+        } else {
+            // 中间点
+            int mid = (left + right) / 2;
+            // 左半边 满足区间和得个数
+            int n1 = countRangeSumRecursive(sum, lower, upper, left, mid);
+            // 右半边 满足区间和得个数
+            int n2 = countRangeSumRecursive(sum, lower, upper, mid + 1, right);
+            int ret = n1 + n2;
+            // 求当前区间满足区间和得个数
+
+            // 首先统计下标对的数量
+            int i = left;
+            int l = mid + 1;
+            int r = mid + 1;
+            while (i <= mid) {
+                // l 右移，当前i到l区间小于low
+                while (l <= right && sum[l] - sum[i] < lower) {
+                    l++;
+                }
+                // r 右移， 当前i到r小于upper
+                while (r <= right && sum[r] - sum[i] <= upper) {
+                    r++;
+                }
+                ret += r - l;
+                i++;
+            }
+
+            // 随后合并两个排序数组
+            int[] sorted = new int[right - left + 1];
+            int p1 = left, p2 = mid + 1;
+            int p = 0;
+            while (p1 <= mid || p2 <= right) {
+                if (p1 > mid) {
+                    sorted[p++] = (int) sum[p2++];
+                } else if (p2 > right) {
+                    sorted[p++] = (int) sum[p1++];
+                } else {
+                    if (sum[p1] < sum[p2]) {
+                        sorted[p++] = (int) sum[p1++];
+                    } else {
+                        sorted[p++] = (int) sum[p2++];
+                    }
+                }
+            }
+            for (int j = 0; j < sorted.length; j++) {
+                sum[left + j] = sorted[j];
+            }
+            return ret;
+        }
+    }
 }
