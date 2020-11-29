0cc71dddf2f603917538abe632ce35ec1931769b
gudongxian <gudongxian@qq.com>
2020-11-29 18:36:25 +0800

    :construction: LeetCode 每日一题 三角形的最大周长

---
+++ LeetCode/src/main/java/cn/dx/leetcode/LargestPerimeter.java
@@ -0,0 +1,27 @@
+package cn.dx.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+ * <p>
+ * 如果不能形成任何面积不为零的三角形，返回 0。
+ * <p>
+ * level: simple
+ *
+ * @author gudongxian
+ * @version 0.1
+ * <p>
+ * 2020/11/29
+ */
+public class LargestPerimeter {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length - 1; i >= 2; --i) {
+            if (A[i - 2] + A[i - 1] > A[i]) {
+                return A[i - 2] + A[i - 1] + A[i];
+            }
+        }
+        return 0;
+    }
+}
