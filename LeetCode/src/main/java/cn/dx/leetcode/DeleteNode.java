df8bfbbc01ad793789479ff97815f0d9adc4b37c
Dongxian Gu <gudongxian@qq.com>
2020-12-03 22:02:19 +0800

    :construction: LeetCode 初级算法 链表

---
+++ LeetCode/src/main/java/cn/dx/leetcode/DeleteNode.java
@@ -0,0 +1,31 @@
+package cn.dx.leetcode;
+
+/**
+ * 删除链表中的节点
+ * <p>
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+ * <p>
+ * 提示：
+ * <p>
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ *
+ * @author dongxian
+ * @version 0.1
+ * @date 2020/12/3
+ **/
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        while (node.next != null) {
+            node.val = node.next.val;
+            if (node.next.next == null) {
+                node.next = null;
+            } else {
+                node = node.next;
+            }
+        }
+
+    }
+}
