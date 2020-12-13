3e40f1e36842896ff6f549a22f90de0b5ea87853
Dongxian Gu <gudongxian@qq.com>
2020-12-13 11:20:12 +0800

    :construction: LeetCode 每日一题 217. 存在重复元素

---
+++ LeetCode/src/main/java/cn/dx/leetcode/ContainDuplicate.java
@@ -0,0 +1,26 @@
+package cn.dx.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * @author dongxian
+ * @version 0.1
+ * @date 2020/12/13
+ **/
+public class ContainDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
