e464993b25f252f41030542a0b2d993e43ab76cd
gudongxian <gudongxian@qq.com>
2020-12-14 15:39:32 +0800

    :construction: LeetCode 每日一题 49. 字母异位词分组

---
+++ LeetCode/src/main/java/cn/dx/leetcode/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package cn.dx.leetcode;
+
+import java.util.*;
+
+/**
+ * 49. 字母异位词分组
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串
+ *
+ * @author gudongxian
+ * @version 0.1
+ * <p>
+ * 2020/12/14
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams ga = new GroupAnagrams();
+        ga.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+    }
+}
