ae4317a3dfd688b8706e05171670f47344816b31
Dongxian Gu <gudongxian@qq.com>
2020-12-15 10:54:05 +0800

    :construction: LeetCode 每日一题 738. 单调递增的数字

---
+++ LeetCode/src/main/java/cn/dx/leetcode/MonotoneIncreasingDigits.java
@@ -0,0 +1,42 @@
+package cn.dx.leetcode;
+
+/**
+ * 给定一个非负整数 N，找出小于或等于 N 的最大的整数，同时这个整数需要满足其各个位数上的数字是单调递增。
+ * <p>
+ * （当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。）
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/monotone-increasing-digits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author dongxian
+ * @version 0.1
+ * @date 2020/12/15
+ **/
+public class MonotoneIncreasingDigits {
+    public int monotoneIncreasingDigits(int N) {
+        char[] strN = Integer.toString(N).toCharArray();
+        int i = 1;
+        while (i < strN.length && strN[i - 1] <= strN[i]) {
+            i += 1;
+        }
+        if (i < strN.length) {
+            while (i > 0 && strN[i - 1] > strN[i]) {
+                strN[i - 1] -= 1;
+                i -= 1;
+            }
+            for (i += 1; i < strN.length; ++i) {
+                strN[i] = '9';
+            }
+        }
+        return Integer.parseInt(new String(strN));
+    }
+
+    public static void main(String[] args) {
+        MonotoneIncreasingDigits mid = new MonotoneIncreasingDigits();
+        System.out.println(mid.monotoneIncreasingDigits(120));
+        System.out.println(mid.monotoneIncreasingDigits(10));
+        System.out.println(mid.monotoneIncreasingDigits(1234));
+        System.out.println(mid.monotoneIncreasingDigits(332));
+    }
+}
