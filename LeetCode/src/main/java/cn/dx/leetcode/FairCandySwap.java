a3fbc22e7b43d7471eec3891bbb35cef0615879e
gudongxian <gudongxian@qq.com>
2021-02-01 11:41:31 +0800

    :construction: LeetCode 每日一题 888. 公平的糖果棒交换

---
+++ LeetCode/src/main/java/cn/dx/leetcode/FairCandySwap.java
@@ -0,0 +1,44 @@
+package cn.dx.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 888. 公平的糖果棒交换
+ * 爱丽丝和鲍勃有不同大小的糖果棒：A[i] 是爱丽丝拥有的第 i 根糖果棒的大小，B[j] 是鲍勃拥有的第 j 根糖果棒的大小。
+ * <p>
+ * 因为他们是朋友，所以他们想交换一根糖果棒，这样交换后，他们都有相同的糖果总量。（一个人拥有的糖果总量是他们拥有的糖果棒大小的总和。）
+ * <p>
+ * 返回一个整数数组 ans，其中 ans[0] 是爱丽丝必须交换的糖果棒的大小，ans[1] 是 Bob 必须交换的糖果棒的大小。
+ * <p>
+ * 如果有多个答案，你可以返回其中任何一个。保证答案存在。
+ * <p>
+ * level: easy
+ *
+ * @author gudongxian
+ * @version 0.1
+ * <p>
+ * 2021/2/1
+ */
+public class FairCandySwap {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int sumA = Arrays.stream(A).sum();
+        int sumB = Arrays.stream(B).sum();
+        int delta = (sumA - sumB) / 2;
+        Set<Integer> rec = new HashSet<>();
+        for (int num : A) {
+            rec.add(num);
+        }
+        int[] ans = new int[2];
+        for (int y : B) {
+            int x = y + delta;
+            if (rec.contains(x)) {
+                ans[0] = x;
+                ans[1] = y;
+                break;
+            }
+        }
+        return ans;
+    }
+}
