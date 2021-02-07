ec0977a32807e755a68c66185b55f48eda1f68e0
Dongxian Gu <gudongxian@qq.com>
2021-02-07 10:02:42 +0800

    :construction: LeetCode 每日一题  665. 非递减数列

---
+++ LeetCode/src/main/java/cn/dx/leetcode/NonDecreasingArray.java
@@ -0,0 +1,30 @@
+package cn.dx.leetcode;
+
+/**
+ * 665. 非递减数列
+ * 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+ * <p>
+ * 我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+ *
+ * @author dongxian
+ * @version 0.1
+ * @date 2021/2/7
+ **/
+public class NonDecreasingArray {
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length, cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
+            }
+        }
+        return true;
+    }
+}
