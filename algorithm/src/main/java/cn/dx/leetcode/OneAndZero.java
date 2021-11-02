863135425ae9a68175903be263d584b307f056f8
gudongxian <gudongxian@qq.com>
2021-11-02 15:12:55 +0800

    :construction: 474. 一和零

---
+++ algorithm/src/main/java/cn/dx/leetcode/OneAndZero.java
@@ -0,0 +1,41 @@
+package cn.dx.leetcode;
+
+/**
+ * 474. 一和零
+ * 给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+ * <p>
+ * 请你找出并返回 strs 的最大子集的长度，该子集中 最多 有 m 个 0 和 n 个 1 。
+ * <p>
+ * 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+ *
+ * @author gudongxian
+ * @date 2021/11/2
+ */
+public class OneAndZero {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int len = strs.length;
+        int[][][] dp = new int[len + 1][m + 1][n + 1];
+        for (int i = 1; i <= len; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (zeros <= j && ones <= k) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[len][m][n];
+    }
+
+    private int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0']++;
+        }
+        return zerosOnes;
+    }
+}
